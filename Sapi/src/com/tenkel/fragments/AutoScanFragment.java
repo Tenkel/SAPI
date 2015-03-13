8e35d894f1547a5b806a1e5d8807f93ce4c466b7
Edu <Edu@192.168.100.2>
2015-03-13 01:22:31 -0300

    Adicionando classe WifiReceiver.

--- Sapi/src/com/tenkel/fragments/AutoScanFragment.java
+++ Sapi/src/com/tenkel/fragments/AutoScanFragment.java
@@ -1,31 +1,41 @@
 package com.tenkel.fragments;
 
-import com.tenkel.sapi.dal.Bridge;
-
-import com.tenkel.sapi.R;
-import com.tenkel.sapi.dal.AndarManager;
-import com.tenkel.sapi.dal.LeituraWifiManager;
-import com.tenkel.sapi.dal.ObservacaoManager;
-import com.tenkel.sapi.dal.PosicaoManager;
-
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.CompoundButton;
-import android.widget.ListView;
 import android.widget.NumberPicker;
 import android.widget.ProgressBar;
 import android.widget.ToggleButton;
 
+import com.tenkel.sapi.R;
+import com.tenkel.sapi.dal.AccessPointManager;
+import com.tenkel.sapi.dal.AndarManager;
+import com.tenkel.sapi.dal.Bridge;
+import com.tenkel.sapi.dal.LeituraWifiManager;
+import com.tenkel.sapi.dal.Observacao;
+import com.tenkel.sapi.dal.ObservacaoManager;
+import com.tenkel.sapi.dal.PosicaoManager;
+
 public class AutoScanFragment extends Fragment {
 
-	private Button captura;
+	private AndarManager mAndarManager;
+	private PosicaoManager mPosicaoManager;
+	private ObservacaoManager mObservacaoManager;
+	private LeituraWifiManager mLeituraWIFIManager;
+	private AccessPointManager mAccessPointManager;
+	
+	
 	private NumberPicker Room;
 	private ProgressBar LoopBar;
 
@@ -65,6 +75,7 @@ public class AutoScanFragment extends Fragment {
 					LoopBar.setVisibility(View.VISIBLE);
 				} 
 				else {
+					Bridge.stop(getActivity());
 					chrono.stop();
 					LoopBar.setVisibility(View.INVISIBLE);
 				}
@@ -73,6 +84,39 @@ public class AutoScanFragment extends Fragment {
 		});
 
         return view;
+	}
+	
+	public class WifiReceiver2 extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.i("RoomActivity", "Processing event from Bridge");
+			
+			long idObservacao = intent.getLongExtra("idObservacao", -1);
+			if (idObservacao == -1) {
+				Log.e("DebugRoomActivity", "Bridge did not send idObservacao, as expected");
+				return;
+			}
+			
+			Object[] objects = (Object[]) intent.getSerializableExtra("wifi");
+			ScanResult[] results = new ScanResult[objects.length];
+			for (int i=0; i<objects.length; ++i) 
+				results[i] = (ScanResult) objects[i];
+			
+			predictPosition(results);
+			
+			if (mState <= 0) {
+				if (!mAndarView.isEnabled()) {
+					Observacao obs = mObservacaoManager.getById(idObservacao);
+					obs.setidPosicao(mPosicaoIds[mAndarView.getSelectedX()][mAndarView.getSelectedY()]);
+					mObservacaoManager.update(obs);
+					
+					mAndarView.addCollected(mAndarView.getSelectedX(), mAndarView.getSelectedY());
+				}
+			} else {
+				mState--;
+			}
+		}
 	}
 
 }
